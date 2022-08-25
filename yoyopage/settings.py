8c82465d8d48a8372b1b2899a1ab02430ef56208
Louis Tsang <kawingt2@gmail.com>
2022-08-25 20:22:42 +0800

    applied base with theme

--- yoyopage/settings.py
+++ yoyopage/settings.py
@@ -108,7 +108,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = 'static/'
+# STATIC_ROOT = "/var/www/example.com/static/"
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
