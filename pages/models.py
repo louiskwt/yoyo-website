756137dea32052f7a6c54e829049ab3a1c986be0
Louis Tsang <kawingt2@gmail.com>
2022-08-28 23:10:19 +0800

    set up db

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class HomePage(models.Model):
+    section_name = models.CharField(max_length=50)
+    links = models.TextField(null=True, blank=True)
+    text_content = models.TextField()
+
+    def __str__(self) -> str:
+        return self.section_name
