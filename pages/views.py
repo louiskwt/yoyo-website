90929e7b9ac57ffc758dc29808be60d177efcca1
Louis Tsang <kawingt2@gmail.com>
2022-08-28 23:47:18 +0800

    set up markdown and render content from db

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import HomePage
 
 # Create your views here.
 def homePageView(request):
-    return render(request, 'pages/home.html')
+    heroContent = HomePage.objects.filter(section_name="hero").values_list('text_content', flat=True)
+   
+    context = {
+        'hero': heroContent[0]
+    }
+    return render(request, 'pages/home.html', context)
