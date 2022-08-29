192cf3daa5bd310106b00d3a35427a050769aae4
Louis Tsang <kawingt2@gmail.com>
2022-08-29 22:59:07 +0800

    render course info

--- pages/admin.py
+++ pages/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import HomePage
+from .models import HomePage, CourseInfo
 
 # Register your models here.
 admin.site.register(HomePage)
+admin.site.register(CourseInfo)
--- pages/views.py
+++ pages/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import HomePage
+from .models import HomePage, CourseInfo
 
 # Create your views here.
 def homePageView(request):
     heroContent = HomePage.objects.filter(section_name="hero").values_list('text_content', flat=True)
-   
+    courseInfo = CourseInfo.objects.all()
+    print(courseInfo)
     context = {
-        'hero': heroContent[0]
+        'hero': heroContent[0],
+        'courseInfoList': courseInfo
     }
     return render(request, 'pages/home.html', context)
