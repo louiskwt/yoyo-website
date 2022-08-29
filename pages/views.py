e6014e2f22ee3a653a6ce65e47a9b0d88b04f6aa
Louis Tsang <kawingt2@gmail.com>
2022-08-29 23:09:05 +0800

    add hero link

--- pages/views.py
+++ pages/views.py
@@ -5,10 +5,12 @@ from .models import HomePage, CourseInfo
 # Create your views here.
 def homePageView(request):
     heroContent = HomePage.objects.filter(section_name="hero").values_list('text_content', flat=True)
+    heroLink = HomePage.objects.filter(section_name="hero").values_list('links', flat=True)
     courseInfo = CourseInfo.objects.all()
-    print(courseInfo)
+    print(heroContent)
     context = {
         'hero': heroContent[0],
+        'link': heroLink[0],
         'courseInfoList': courseInfo
     }
     return render(request, 'pages/home.html', context)
