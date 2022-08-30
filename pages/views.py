ba9895ed5ed30ec45f9478a1b60445f01a27ba74
Louis Tsang <kawingt2@gmail.com>
2022-08-30 23:38:20 +0800

    render contact from db

--- pages/views.py
+++ pages/views.py
@@ -1,15 +1,21 @@
 from django.shortcuts import render
-from .models import HomePage, CourseInfo
+from .models import Contact, HomePage, CourseInfo
 
 # Create your views here.
 def homePageView(request):
     heroContent = HomePage.objects.filter(section_name="hero").values_list('text_content', flat=True)
+
     heroLink = HomePage.objects.filter(section_name="hero").values_list('links', flat=True)
+
     courseInfo = CourseInfo.objects.all()
+
+    contactList = Contact.objects.all()
+
     print(heroContent)
     context = {
         'hero': heroContent[0],
         'link': heroLink[0],
-        'courseInfoList': courseInfo
+        'courseInfoList': courseInfo,
+        'contactList': contactList
     }
     return render(request, 'pages/home.html', context)
